76f6dab107e64adb4fae25913af720e993301829
2kevink <kevink2@yandex.ru>
2018-09-11 03:17:47 +0300

    '3'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.mas = [];
+    this._length = 0;
+    this.compareFunction = function(x,y){return x-y;};
   }
 
   add(element) {
-    // your implementation
+    this.mas.push(element);
+    this._length++;
   }
 
   at(index) {
-    // your implementation
+    return this.mas[index];
   }
 
   get length() {
-    // your implementation
+    return this._length;
   }
 
   toArray() {
-    // your implementation
+    return this.mas;
   }
 
   sort(indices) {
-    // your implementation
+    
+    var i=0;
+    var tmp = [];
+    for(i=0; i<indices.length; i++){
+      tmp.push(this.mas[indices[i]]);
+    }
+    
+    tmp.sort(this.compareFunction);
+    
+    indices.sort(function(x,y){return x-y});
+    for(i=0; i<indices.length; i++){
+      this.mas[indices[i]] = tmp[i];
+    }
+    
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
